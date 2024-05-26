ed815991adeb15b30f17edd29d6997aa118e23a9
Timm TheBlokker <theblokker.dev@gmail.com>
2024-05-26 09:59:21 +0200

    adding c# support

---
+++ mainserver/backend/tr-world/tr-world/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AltV.Net;
+
+namespace tr_world
+{
+    public class Main : Resource
+    {
+        public override void OnStart()
+        {
+            Alt.Log("Server-c#-backend is starting!");
+
+        }
+
+        public override void OnStop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
