4cca0085534c181e9a7b5ad755cf952e4107ad54
Timm TheBlokker <theblokker.help@gmail.com>
2024-06-14 13:57:46 +0200

    adding events for joining, default ChatApi

---
+++ mainserver/backend/tr-world/tr-world/Events.cs
@@ -0,0 +1,21 @@
+﻿using AltV.Net;
+using AltV.Net.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tr_world.Base;
+
+namespace tr_world
+{
+    internal class Events : IScript
+    {
+        [ScriptEvent(ScriptEventType.PlayerConnect)]
+        public void PlayerConnect(BPlayer player)
+        {
+            player.Model = (uint)PedModel.FreemodeMale01;
+            player.Spawn(new AltV.Net.Data.Position(0, 0, 98), 0);
+        }
+    }
+}
