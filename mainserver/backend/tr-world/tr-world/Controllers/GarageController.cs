cd9130685db098da64410f97061017b472bd3a8f
Timm TheBlokker <theblokker.dev@gmail.com>
2024-12-18 12:28:17 +0100

    adding Garage Controller

--- mainserver/backend/tr-world/tr-world/Controllers/GarageController.cs
+++ mainserver/backend/tr-world/tr-world/Controllers/GarageController.cs
@@ -4,22 +4,26 @@ using tr_world.Player;
 
 namespace tr_world.Controllers;
 
-internal class GarageController : IScript
+public class GarageController : IScript
 {
     public static void GetGarageForPlayer(BPlayer player)
     {
+        
     }
 
     public static void SaveGarageForPlayer(BPlayer player)
     {
+        
     }
 
     public static void UpdateGarageForPlayer(BPlayer player)
     {
+        
     }
 
     [Obsolete]
     public static void GetTheListOfVehFromPlayer(BPlayer player)
     {
+        
     }
 }
